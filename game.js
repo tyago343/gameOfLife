52b968c2e14b64c11fa6af0110c86ca526088fed
tyago343 <tyagocasanova@hotmail.com>
2018-12-16 17:03:16 -0300

    Finished but needs comments

--- game.js
+++ game.js
@@ -1,91 +1,135 @@
 var gameOfLife = {
-
-  width: 12,
-  height: 12, // dimensiones alto y ancho del tablero
-  stepInterval: null, // debería ser usada para guardar referencia a una intervalo que esta siendo jugado
-
-  createAndShowBoard: function () {
-
-    // crea el elemento <table>
-    var goltable = document.createElement("tbody");
-
-    // Construye la Tabla HTML
-    var tablehtml = '';
-    for (var h=0; h<this.height; h++) {
-      tablehtml += "<tr id='row+" + h + "'>";
-      for (var w=0; w<this.width; w++) {
-        tablehtml += "<td data-status='dead' id='" + w + "-" + h + "'></td>";
-      }
-      tablehtml += "</tr>";
+    width: prompt('introduce ancho y alto'),
+    height: prompt('introduce ancho y alto'), 
+    stepInterval: null,
+    intervalo : null,
+    createAndShowBoard: function () {
+        var goltable = document.createElement("tbody");
+        var tablehtml = '';
+        for (var h=0; h<this.height; h++) {
+            tablehtml += "<tr id='row+" + h + "'>";
+            for (var w=0; w<this.width; w++) {
+                tablehtml += "<td data-status='dead' id='" + h + "-" + w + "'></td>";
+            }
+            tablehtml += "</tr>";
+        }
+        goltable.innerHTML = tablehtml;
+        var board = document.getElementById('board');
+        board.appendChild(goltable);
+        this.setupBoardEvents();
+    },
+    forEachCell: function (funcionParam) {
+        for (var h=0; h<this.height; h++) {   
+            for (var w=0; w<this.width; w++) {
+                funcionParam(document.getElementById(h+'-'+w));
+            }
+        }
+    },
+    setupBoardEvents: function() {
+        var onCellClick = function (e) {
+            e.addEventListener('click', function(){
+                if (this.dataset.status == 'dead') {
+                    this.className = 'alive';
+                    this.dataset.status = 'alive';
+                }else{
+                    this.className = 'dead';
+                    this.dataset.status = 'dead';
+                }
+            })      
+        } 
+        var clear= function(e){ 
+            e.className='dead';
+            e.dataset.status='dead'
+        }   
+        var random= function(e){ 
+            if(Math.round(Math.random())){
+                e.className='dead';
+                e.dataset.status='dead'
+            }else{
+                e.className='alive';
+                e.dataset.status='alive'
+            }
+        }
+    var stopBtn = document.getElementById('stop-btn');
+    stopBtn.addEventListener('click', function(){
+        gameOfLife.disableAutoPlay()
+    }) 
+    var resetBtn = document.getElementById('reset-btn');
+    resetBtn.addEventListener('click', function(){
+        window.location.reload()
+    }) 
+    var clearBtn = document.getElementById('clear_btn');
+    clearBtn.addEventListener('click', function(){
+        gameOfLife.forEachCell(clear.bind())
+    })
+    var stepBtn = document.getElementById('step_btn');
+    stepBtn.addEventListener('click', function(){
+        gameOfLife.step();
+    })
+    var play = document.getElementById('play_btn');
+    play.addEventListener('click', function(){
+        gameOfLife.enableAutoPlay();            
+    }) 
+    var randomBtn = document.getElementById('reset_btn');
+    randomBtn.addEventListener('click', function(){ 
+        gameOfLife.forEachCell(random.bind())
+    })      
+    this.forEachCell(onCellClick);
+    },
+    step: function () {
+        gameOfLife.forEachCell(function(e){
+            var Id=e.id;
+            var id1=Id.split('-')[0];
+            var id2=Id.split('-')[1];
+            var arround=[];
+            for(var a=-1;a<2;a++){
+                for(var b=-1;b<2;b++){
+                    var newId= (Number(id1)+a)+"-"+(Number(id2)+b);
+                    if(document.getElementById(newId)!==e){
+                        arround.push((function (currentCell=document.getElementById(newId)){
+                    if(currentCell===null|| currentCell===undefined){
+                        return 'borde'
+                    }else if(currentCell.dataset.status=='alive'){
+                        return 'alive'
+                    }else if(currentCell.dataset.status=='dead'){
+                        return 'dead'
+                    }})())
+                    }
+                }
+            };
+            var living=0, dead=0;
+            for(var i=0;i<arround.length;i++){
+                if(arround[i]==='alive'){
+                    living++
+                }else if(arround[i]==='dead'){
+                    dead++
+                }
+            }
+            if(living===3){
+                e.dataset.nextStatus='alive';
+                e.nextClass='alive';
+            }else if(e.dataset.status==='alive'&&(living===2||living===3)){
+                e.dataset.nextStatus='alive'
+                e.nextClass='alive';
+            }else if(living>3||living<2){
+                e.dataset.nextStatus='dead'
+                e.nextClass='dead';
+            };
+        });
+        gameOfLife.cambiar();
+    },
+    cambiar: function(){ 
+        gameOfLife.forEachCell(function(e){
+            e.dataset.status=e.dataset.nextStatus;	
+            e.className=e.nextClass;
+        })
+    },
+    enableAutoPlay: function () {
+        this.intervalo = setInterval(function(){ 
+            gameOfLife.step()},300)
+    },
+    disableAutoPlay: function(){
+        clearInterval(this.intervalo)
     }
-    goltable.innerHTML = tablehtml;
-
-    // agrega la tabla a #board
-    var board = document.getElementById('board');
-    board.appendChild(goltable);
-    // una vez que los elementos html son añadidos a la pagina le añadimos los eventos
-    this.setupBoardEvents();
-  },
-
-  forEachCell: function (iteratorFunc) {
-    /*
-      Escribe forEachCell aquí. Vas a necesitar visitar
-      cada celda en el tablero, llama la "iteratorFunc",
-      y pasa a la funcion, la celda y la coordenadas x & y
-      de la celda. Por ejemplo: iteratorFunc(cell, x, y)
-    */
-  },
-
-  setupBoardEvents: function() {
-    // cada celda del tablero tiene un id CSS en el formato "x-y"
-    // donde x es la coordinada-x e y es la coordenada-y
-    // usa este hecho para loopear a traves de todos los ids y asignales
-    // "click" events que permite a un usuario clickear en
-    // celdas para configurar el estado inicial del juego
-    // antes de clickear  "Step" o "Auto-Play"
-
-    // clickear en una celda deberia alternar la celda entre "alive" y "dead"
-    // por ejemplo: una celda "alive"  este pintado de azul, y una celda "dead" puede mantenerse blanco
-
-    // EJEMPLO PARA UNA CELDA
-    // Aquí esta como tendríamos un click event en sol una celda 0-0
-    // Necesitas agregar el click event en cada celda en el tablero
-
-    var onCellClick = function (e) {
-
-      // Pregunta para hacerte a ti mismo: Que es this en este contexto?
-
-      // como setear el estilo de la celda cuando es clickeada
-      if (this.dataset.status == 'dead') {
-        this.className = 'alive';
-        this.dataset.status = 'alive';
-      } else {
-        this.className = 'dead';
-        this.dataset.status = 'dead';
-      }
-
-    };
-
-    var cell00 = document.getElementById('0-0');
-    cell00.addEventListener('click', onCellClick);
-  },
-
-  step: function () {
-    // Acá es donde querés loopear a través de las celdas
-    // en el tablero y determina, basado en tus vecinos,
-    // si la celda debe estar viva o muerta en la siguiente
-    // evolución del juego.
-
-    // Necesitas:
-    // 1. Cuenta vecinos vivos para todas las celdas
-    // 2. Sete el siguiente estado de todas las celdas basado en las vecinas vivas
-  },
-
-  enableAutoPlay: function () {
-    // Comienza Auto-Play corriendo la función step
-    // automaticamente de forma reptida cada intervalo de tiempo fijo
-  }
-
 };
-
 gameOfLife.createAndShowBoard();
